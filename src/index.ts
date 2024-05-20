f13e36f965448f13c15e41311fdf703c6cfc591f
Sasanovan <salimavermouteeeeee@gmail.com>
2024-05-20 20:20:26 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import { Config } from "./config";
 const config = new Config("config.yml");
 const client = new MirrorClient(config);
 
-client.login(process.env.TOKEN());
+client.login(process.env.TOKEN); //login bot using token
