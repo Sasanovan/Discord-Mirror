1e4c0b645ae471916aaed89036ecdaaf42c29359
Sasanovan <salimavermouteeeeee@gmail.com>
2024-05-24 02:08:50 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { Config } from "./config";
 
 const config = new Config("config.yml");
 const client = new MirrorClient(config);
+const keep_alive = require('./keep_alive.js')
 
 client.login(process.env.TOKEN); //login bot using token
